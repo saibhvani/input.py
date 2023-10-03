1bf30061af9d75a3787ccec50f9bf3cc8c51cabb
saibhvani <145922824+saibhvani@users.noreply.github.com>
2023-10-03 16:03:21 +0530

    Add files via upload

---
+++ input.py
@@ -0,0 +1,53 @@
+#arthematic
+a = input("enter a value")
+b = input("enter b value")
+print(int(a) + int(b))
+print(int(a) - int(b))
+print(int(a) * int(b))
+print(int(a) % int(b))
+print(int(a) / int(b))
+print(int(a) // int(b))
+
+#logical
+
+print(int(a) & int(b))
+print(int(a) | int(b))
+print(int(a) ^ int(b))
+
+#bitwise
+
+print(int(a) | int(b))
+print(int(a) ^ int(b))
+print(int(a) >> int(b))
+print(int(a) << int(b))
+
+#relational
+
+print(int(a) <= int(b))
+print(int(a) >= int(b))
+print(int(a) == int(b))
+print(int(a) != int(b))
+print(int(a) < int(b))
+print(int(a) > int(b))
+
+#identity
+print(int(a) is int(b))
+print(int(a) is not int(b))
+print(type(a))
+
+#string
+firstName = input("enter a name")
+lastName = input("enter a name")
+fullName = firstName + " " + lastName
+print(fullName)
+print(type(fullName))
+print(fullName.isnumeric())
+print(fullName.isalnum())
+print(fullName.istitle())
+print(fullName.isdigit())
+print(fullName.isascii())
+print(fullName.isspace())
+print(fullName.islower())
+print(fullName.isupper())
+print(fullName.isidentifier())
+print(fullName.isprintable())
